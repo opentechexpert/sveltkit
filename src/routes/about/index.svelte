0fcff1142cb96dcaf7f47a8b594971b8cefbef62
Opentech <github@opentech.expert.com>
2022-02-13 17:52:17 +0000

    add about and amp routes

---
+++ src/routes/about/index.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+	export const prerender = true;
+</script>
+<h1>About SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/amp/index.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+	// export const amp = true;
+</script>
+<h1>AMP Page</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
