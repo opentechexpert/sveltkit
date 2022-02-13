fcebc1819b9c9509c309cdbd90243af86e3fce91
Opentech <github@opentech.expert.com>
2022-02-13 17:51:52 +0000

    add adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
+
 import preprocess from 'svelte-preprocess';
+
+const adapt = process.env.ADAPTER;
+const adapters = adapt == "static" ? staticAdapter : adapter;
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +14,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-		// amp: true,
+		adapter: adapters(),
 	}
 };
 
